73e19e3e35c8cd00de0047f403b40d9987468999
nekko-master <kanomaster00@gmail.com>
2025-10-31 17:19:09 +0900

    結合テスト_異常系

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe 'ユーザー新規登録', type: :system do
+RSpec.describe 'ユーザー新規登録/ログイン（正常系）', type: :system do
   before do
     @user = FactoryBot.build(:user)
   end
 
-  context 'トップページ' do
-    it 'ログアウト状態の場合には、「新規登録」「ログイン」ボタンが表示される' do
+  context "ユーザーの新規登録ができる
+  ユーザーのログイン/ログアウトができる" do
+
+    it "ログアウト状態の場合には、「新規登録」「ログイン」ボタンが表示される" do
       visit root_path
       expect(page).to have_content('新規登録')
       expect(page).to have_content('ログイン')
@@ -29,10 +31,10 @@ RSpec.describe 'ユーザー新規登録', type: :system do
       select '2000', from: 'user[birthday(1i)]'
       select '1', from: 'user[birthday(2i)]'
       select '1', from: 'user[birthday(3i)]'
-      expect do
+      expect{
         find('input[name="commit"]').click
         sleep 1
-      end.to change { User.count }.by(1)
+      }.to change { User.count }.by(1)
 
       # ログイン状態の場合には、「ユーザーのニックネーム」と「ログアウト」ボタンが表示される
       expect(page).to have_content(@user.nickname)
@@ -48,10 +50,58 @@ RSpec.describe 'ユーザー新規登録', type: :system do
       fill_in 'email', with: @user.email
       fill_in 'password', with: @user.password
       find('input[name="commit"]').click
-
+      
       expect(page).to have_content(@user.nickname)
       expect(page).to have_content('ログアウト')
       expect(page).to have_no_content('ログイン')
+
+      
+
+    end
+  end
+end
+
+RSpec.describe 'ユーザー新規登録/ログイン（異常系）', type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  context "ユーザーの新規登録ができずエラーメッセージが出力される" do
+
+    it "「新規登録」ボタンをクリックすると、新規登録ページに遷移できる
+    入力に誤りがあるとエラーメッセージが出る" do
+      visit root_path
+      click_link '新規登録'
+      expect(page).to have_current_path(new_user_registration_path)
+      fill_in 'nickname', with: ''
+      fill_in 'email', with: ''
+      fill_in 'password', with: ''
+      fill_in 'password-confirmation', with: ''
+      fill_in 'last-name', with: ''
+      fill_in 'first-name', with: ''
+      fill_in 'last-name-kana', with: ''
+      fill_in 'first-name-kana', with: ''
+      select '2001', from: 'user[birthday(1i)]'
+      select '2', from: 'user[birthday(2i)]'
+      select '2', from: 'user[birthday(3i)]'
+
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらない
+      expect{
+        find('input[name="commit"]').click
+        sleep 1
+      }.to change { User.count }.by(0)
+    end
+  end
+
+  context "ログインができずエラーメッセージが出力される" do
+
+    it "入力に誤りがあるとエラーメッセージが出る" do
+      visit root_path
+      click_link 'ログイン'
+      expect(page).to have_current_path(new_user_session_path)
+      fill_in 'email', with: ""
+      fill_in 'password', with: ""
+      find('input[name="commit"]').click
     end
   end
 end
