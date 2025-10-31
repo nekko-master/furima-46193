b4a149f3814aa0ea96d94216baace5fe08d1768d
nekko-master <kanomaster00@gmail.com>
2025-10-31 15:22:55 +0900

    結合テスト_正常系

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -11,33 +11,48 @@ RSpec.describe 'ユーザー新規登録', type: :system do
       expect(page).to have_content('新規登録')
       expect(page).to have_content('ログイン')
     end
-    it "「新規登録」ボタンをクリックすると、新規登録ページに遷移しユーザー新規登録できる" do
+    it "「新規登録」ボタンをクリックすると、新規登録ページに遷移しユーザー新規登録できる
+    ログイン状態の場合には、「ユーザーのニックネーム」と「ログアウト」ボタンが表示される
+    「ログアウト」ボタンをクリックすると、ログアウトができる
+    「ログイン」ボタンをクリックすると、ログインができる" do
+      visit root_path
       click_link '新規登録'
-      expect(page).to have_current_path('new_user_registration_path')
-      # fill_in 'Nickname', with: @user.nickname
-      # fill_in 'Email', with: @user.email
-      # fill_in 'Password', with: @user.password
-      # fill_in 'Last_name', with: @user.last_name
-      # fill_in 'First_name', with: @user.first_name
-      # fill_in 'Last_name_kana', with: @user.last_name_kana
-      # fill_in 'First_name_kana', with: @user.first_name_kana
-      # fill_in 'Birthday', with: @user.birthday
-      # expect{
-      #   find('input[name="commit"]').click
-      #   sleep 1
-      # }.to change { User.count }.by(1)
-    end
+      expect(page).to have_current_path(new_user_registration_path)
+      fill_in 'nickname', with: @user.nickname
+      fill_in 'email', with: @user.email
+      fill_in 'password', with: @user.password
+      fill_in 'password-confirmation', with: @user.password
+      fill_in 'last-name', with: @user.last_name
+      fill_in 'first-name', with: @user.first_name
+      fill_in 'last-name-kana', with: @user.last_name_kana
+      fill_in 'first-name-kana', with: @user.first_name_kana
+      select '2000', from: 'user[birthday(1i)]'
+      select '1', from: 'user[birthday(2i)]'
+      select '1', from: 'user[birthday(3i)]'
+      expect{
+        find('input[name="commit"]').click
+        sleep 1
+      }.to change { User.count }.by(1)
 
-#    it "ログイン状態の場合には、「ユーザーのニックネーム」と「ログアウト」ボタンが表示される" do
-#      expect(page).to have_content(@user.nickname)
-#      expect(page).to have_content('ログアウト')
-#    end
-#     it "「新規登録」「ログイン」ボタンをクリックすると、各ページに遷移できる" do
-# find('新規登録').click expect(page).to have_current_path('new_user_registration_path')
-# find('ログイン').click expect(page).to have_current_path('new_user_session_path')
-#     end
-#     it "「ログアウト」ボタンをクリックすると、ログアウトができる" do
-# find('新規登録').click expect(page).to have_current_path('new_user_session_path')
-#     end
+      # ログイン状態の場合には、「ユーザーのニックネーム」と「ログアウト」ボタンが表示される
+      expect(page).to have_content(@user.nickname)
+      expect(page).to have_content('ログアウト')
+
+      # 「ログアウト」ボタンをクリックすると、ログアウトができる
+      click_link 'ログアウト'
+      expect(page).to have_current_path(root_path)
+
+      # 「ログイン」ボタンをクリックすると、ログインができる
+      click_link 'ログイン'
+      expect(page).to have_current_path(new_user_session_path)
+      fill_in 'email', with: @user.email
+      fill_in 'password', with: @user.password
+      find('input[name="commit"]').click
+      
+      expect(page).to have_content(@user.nickname)
+      expect(page).to have_content('ログアウト')
+      expect(page).to have_no_content('ログイン')
+
+    end
   end
 end
