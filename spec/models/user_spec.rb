f096d311fbf181a2775080d25bd6c56ecd529d32
nekko-master <kanomaster00@gmail.com>
2025-10-31 15:28:13 +0900

    Rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,51 +5,51 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-  context "ユーザー新規登録できる場合" do
-    it "nickname、email、password、名前、名前カナ、生年月日があれば登録できる" do
+  context 'ユーザー新規登録できる場合' do
+    it 'nickname、email、password、名前、名前カナ、生年月日があれば登録できる' do
       expect(@user).to be_valid
     end
   end
 
-  context "新規登録/ユーザー情報" do
-    it "nicknameが空では登録できない" do
+  context '新規登録/ユーザー情報' do
+    it 'nicknameが空では登録できない' do
       @user.nickname = ' '
       @user.valid?
       expect(@user.errors.full_messages).to include "Nickname can't be blank"
     end
-    it "emailが空では登録できない" do
+    it 'emailが空では登録できない' do
       @user.email = ' '
       @user.valid?
       expect(@user.errors.full_messages).to include "Email can't be blank"
     end
-    it "emailが重複すると登録できない" do
+    it 'emailが重複すると登録できない' do
       @user.save
       another_user = FactoryBot.build(:user)
       another_user.email = @user.email
       another_user.valid?
-      expect(another_user.errors.full_messages).to include "Email has already been taken"
+      expect(another_user.errors.full_messages).to include 'Email has already been taken'
     end
-    it "emailは@を含まないと登録できない" do
+    it 'emailは@を含まないと登録できない' do
       @user.email = 'abcdefg'
       @user.valid?
-      expect(@user.errors.full_messages).to include "Email is invalid"
+      expect(@user.errors.full_messages).to include 'Email is invalid'
     end
-    it "passwordが空では登録できない" do
+    it 'passwordが空では登録できない' do
       @user.password = ' '
       @user.valid?
       expect(@user.errors.full_messages).to include "Password can't be blank"
     end
-    it "passwordが5文字以下では登録できない" do
+    it 'passwordが5文字以下では登録できない' do
       @user.password = '1234a'
       @user.valid?
-      expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+      expect(@user.errors.full_messages).to include 'Password is too short (minimum is 6 characters)'
     end
-    it "passwordは半角英数字混合でないと登録できない" do
+    it 'passwordは半角英数字混合でないと登録できない' do
       @user.password = '123456'
       @user.valid?
       expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
     end
-    it "passwordとpassword_confirmationが不一致では登録できない" do
+    it 'passwordとpassword_confirmationが不一致では登録できない' do
       @user.password = '12345a'
       @user.password_confirmation = '12345b'
       @user.valid?
@@ -57,52 +57,51 @@ RSpec.describe User, type: :model do
     end
   end
 
-  context "新規登録/本人情報確認" do
-    it "last_nameが空では登録できない" do
+  context '新規登録/本人情報確認' do
+    it 'last_nameが空では登録できない' do
       @user.last_name = ' '
       @user.valid?
       expect(@user.errors.full_messages).to include "Last name can't be blank"
     end
-    it "last_nameは全角でないと登録できない" do
+    it 'last_nameは全角でないと登録できない' do
       @user.last_name = 'ﾔﾏﾀﾞ'
       @user.valid?
-      expect(@user.errors.full_messages).to include "Last name is invalid"
+      expect(@user.errors.full_messages).to include 'Last name is invalid'
     end
-    it "first_nameが空では登録できない" do
+    it 'first_nameが空では登録できない' do
       @user.first_name = ' '
       @user.valid?
       expect(@user.errors.full_messages).to include "First name can't be blank"
     end
-    it "first_nameは全角でないと登録できない" do
+    it 'first_nameは全角でないと登録できない' do
       @user.first_name = 'ﾀﾛｳ'
       @user.valid?
-      expect(@user.errors.full_messages).to include "First name is invalid"
+      expect(@user.errors.full_messages).to include 'First name is invalid'
     end
-    it "last_name_kanaが空では登録できない" do
+    it 'last_name_kanaが空では登録できない' do
       @user.last_name_kana = ' '
       @user.valid?
       expect(@user.errors.full_messages).to include "Last name kana can't be blank"
     end
-    it "last_name_kanaは全角（カタカナ）でないと登録できない" do
+    it 'last_name_kanaは全角（カタカナ）でないと登録できない' do
       @user.last_name_kana = 'ﾔﾏﾀﾞ'
       @user.valid?
-      expect(@user.errors.full_messages).to include "Last name kana is invalid"
+      expect(@user.errors.full_messages).to include 'Last name kana is invalid'
     end
-    it "first_name_kanaが空では登録できない" do
+    it 'first_name_kanaが空では登録できない' do
       @user.first_name_kana = ' '
       @user.valid?
       expect(@user.errors.full_messages).to include "First name kana can't be blank"
     end
-    it "first_name_kanaは全角（カタカナ）でないと登録できない" do
+    it 'first_name_kanaは全角（カタカナ）でないと登録できない' do
       @user.first_name_kana = 'ﾀﾛｳ'
       @user.valid?
-      expect(@user.errors.full_messages).to include "First name kana is invalid"
+      expect(@user.errors.full_messages).to include 'First name kana is invalid'
     end
-    it "birthdayが空では登録できない" do
+    it 'birthdayが空では登録できない' do
       @user.birthday = ' '
       @user.valid?
       expect(@user.errors.full_messages).to include "Birthday can't be blank"
     end
   end
-
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe 'ユーザー新規登録', type: :system do
     @user = FactoryBot.build(:user)
   end
 
-  context "トップページ" do
-    it "ログアウト状態の場合には、「新規登録」「ログイン」ボタンが表示される" do
+  context 'トップページ' do
+    it 'ログアウト状態の場合には、「新規登録」「ログイン」ボタンが表示される' do
       visit root_path
       expect(page).to have_content('新規登録')
       expect(page).to have_content('ログイン')
@@ -29,10 +29,10 @@ RSpec.describe 'ユーザー新規登録', type: :system do
       select '2000', from: 'user[birthday(1i)]'
       select '1', from: 'user[birthday(2i)]'
       select '1', from: 'user[birthday(3i)]'
-      expect{
+      expect do
         find('input[name="commit"]').click
         sleep 1
-      }.to change { User.count }.by(1)
+      end.to change { User.count }.by(1)
 
       # ログイン状態の場合には、「ユーザーのニックネーム」と「ログアウト」ボタンが表示される
       expect(page).to have_content(@user.nickname)
@@ -48,11 +48,10 @@ RSpec.describe 'ユーザー新規登録', type: :system do
       fill_in 'email', with: @user.email
       fill_in 'password', with: @user.password
       find('input[name="commit"]').click
-      
+
       expect(page).to have_content(@user.nickname)
       expect(page).to have_content('ログアウト')
       expect(page).to have_no_content('ログイン')
-
     end
   end
 end
