6d9c56811e84b58b6c98253ad2615a6bd5cfd420
nekko-master <kanomaster00@gmail.com>
2025-11-11 16:28:16 +0900

    DBへ出品情報の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create
-    redirect_to '/'
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :explanation, :price, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,17 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true
+
+  belongs_to :user
+
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :shipping_day
+
+  has_one_attached :image
 end
