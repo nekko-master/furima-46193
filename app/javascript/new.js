3858488a9d54cbd33cf13bb1e02aabe19170ace1
nekko-master <kanomaster00@gmail.com>
2025-11-12 23:20:25 +0900

    JavaScript画面表示

---
+++ app/javascript/new.js
@@ -0,0 +1,20 @@
+document.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+
+  if (priceInput){
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+
+      const addTaxDom = document.getElementById("add-tax-price");
+      const profitDom = document.getElementById("profit");
+
+      const tax = Math.floor(inputValue * 0.1);
+      const profit = Math.floor(inputValue - tax);
+
+      addTaxDom.innerHTML = tax;
+      profitDom.innerHTML = profit;
+    });
+  }
+});
+
+
