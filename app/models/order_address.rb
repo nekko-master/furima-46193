bdbc61404c38d5820b45f7429ecd632913a49fce
nekko-master <kanomaster00@gmail.com>
2025-12-04 18:14:35 +0900

    購入機能単体テスト

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -11,6 +11,7 @@ class OrderAddress
     validates :municipalities
     validates :street_address
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :token
   end
 
   validates :prefecture_id, numericality: { other_than: 1 }
